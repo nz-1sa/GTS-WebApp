0cbddba614a660ca3e08d10f7764386c33ea9f2a
Sean <sean@gtsnz.com>
2022-10-14 18:37:14 +1300

    new handlers now register for responding to post actions

--- dist/gts.webserver.js
+++ dist/gts.webserver.js
@@ -61,7 +61,7 @@ class WebServerHelper {
     // register how to hanle a web request; the url to listen on, required parameters to be sent, and the function to do. Params are not checked
     registerHandlerUnchecked(webapp, url, requiredParams, work) {
         return __awaiter(this, void 0, void 0, function* () {
-            webapp.get(url, (req, res) => __awaiter(this, void 0, void 0, function* () {
+            webapp.post(url, (req, res) => __awaiter(this, void 0, void 0, function* () {
                 yield this.handleRequestUnchecked(req, res, url, requiredParams, work);
             }));
         });
--- lib/gts.webserver.ts
+++ lib/gts.webserver.ts
@@ -37,7 +37,7 @@ export class WebServerHelper{
 	
 	// register how to hanle a web request; the url to listen on, required parameters to be sent, and the function to do. Params are not checked
 	public async registerHandlerUnchecked(webapp:Express.Application, url:string, requiredParams:string[], work:Function):Promise<void>{
-		webapp.get(url, async (req:Express.Request, res:Express.Response) =>{
+		webapp.post(url, async (req:Express.Request, res:Express.Response) =>{
 			await this.handleRequestUnchecked(req, res, url, requiredParams, work);
 		});
 	}
