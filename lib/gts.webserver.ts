ce7e4fc6372a9b3ea0659eb4ecb3f62e2d4f2c85
Sean <sean@gtsnz.com>
2023-04-01 10:25:33 +1300

    d

--- lib/gts.webserver.ts
+++ lib/gts.webserver.ts
@@ -311,9 +311,7 @@ export class WebServerHelper{
 						let isLoggedIn:boolean = await Secure.Session.isLoggedIn(uuid, requestIp, cookies);
 						if(!isLoggedIn){
 							res.status(401);
-							let resp401:WebResponse = await this.handleServeFile(web, res, '/admin/401', uuid, {uuid:uuid, requestIp:requestIp, cookies:cookies, url:url, sessionId:'', isLoggedIn:isLoggedIn, data:{}});
-							res.send(resp401.data);
-							res.end();
+							this.handleServeFile(web, res, '/admin/401', uuid, {uuid:uuid, requestIp:requestIp, cookies:cookies, url:url, sessionId:'', isLoggedIn:isLoggedIn, data:{}});
 							resp = new WebResponse(true, '401 Unauthorised',`UUID:${uuid} Trying to access admin without login`,'');
 						} else {
 							console.log('process admin file request '+url);
